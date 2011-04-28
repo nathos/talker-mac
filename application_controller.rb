30083e32853e5c4ea1e877485c594c2d6173296e
Pablo Villalba <pablo@teambox.com>
2011-04-28 07:24:53 -0700

    Adding gitignore, remove build folder

--- application_controller.rb
+++ application_controller.rb
@@ -1,27 +1,37 @@
-require 'rack_url_protocol'
-
 class ApplicationController
   attr_accessor :webView
   attr_accessor :sidebarView
 
   # fields
   attr_accessor :first_name
+  
+  attr_accessor :messageForm
 
   def awakeFromNib
 	webView.mainFrameURL = "http://asdasdasda.lvh.me:3000/rooms/1059638631"
 	sidebarView.action = "edit:"
-	retrieve_names
+	puts messageForm.inspect
+	messageForm.action = "send_message:"
+	populate_sidebar
+  end
+
+  def send_message(sender)
+	if messageForm.stringValue
+  	  @js = webView.windowScriptObject
+	  @js.evaluateWebScript("Talker.sendMessage('"+messageForm.stringValue+"')")
+	  messageForm.stringValue = ""
+	end
   end
 
   def edit(sender)
 	puts "click"
   end
 
-  def retrieve_names
+  def populate_sidebar
     @names = ["My rooms", "Barcelona", "Hong Kong"]
     sidebarView.dataSource = self
   end
-  
+
   def numberOfRowsInTableView(view)
     @names.size
   end
