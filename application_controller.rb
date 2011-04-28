b59a854be1936ae326ff355e7da5101dd86debc5
Pablo Villalba <pablo@teambox.com>
2011-04-28 16:14:03 -0700

    Adding icon and Growl framework

--- application_controller.rb
+++ application_controller.rb
@@ -1,34 +1,54 @@
+require 'json'
+
 class ApplicationController
+  # Main web view and sidebar
   attr_accessor :webView
   attr_accessor :sidebarView
 
-  # fields
+  # Fields for the sidebar
   attr_accessor :first_name
   
+  # Form to send messages
   attr_accessor :messageForm
 
+  # Initialize the app...
   def awakeFromNib
+
 	webView.mainFrameURL = "http://asdasdasda.lvh.me:3000/rooms/1059638631"
+
+	# Selecting an element on the sidebar
 	sidebarView.action = "edit:"
-	puts messageForm.inspect
+
+	# Hitting <return> on the message form will post a message
 	messageForm.action = "send_message:"
+
+	# Load the data for the sidebar
 	populate_sidebar
+	GrowlApplicationBridge.setGrowlDelegate(self)
   end
 
+  # Post a message to the room using JS in the room itself
   def send_message(sender)
-	if messageForm.stringValue
+	if messageForm.stringValue && messageForm.stringValue.length > 0
+	  puts messageForm.stringValue
   	  @js = webView.windowScriptObject
-	  @js.evaluateWebScript("Talker.sendMessage('"+messageForm.stringValue+"')")
+	  @js.evaluateWebScript("Talker.sendMessage(#{messageForm.stringValue.to_json})")
 	  messageForm.stringValue = ""
 	end
   end
 
   def edit(sender)
 	puts "click"
+	case sidebarView.selectedRow
+	when 0
+	  webView.mainFrameURL = "http://asdasdasda.lvh.me:3000/rooms/1059638631"
+	when 1
+	  webView.mainFrameURL = "https://teambox.talkerapp.com/rooms/819"
+	end
   end
 
   def populate_sidebar
-    @names = ["My rooms", "Barcelona", "Hong Kong"]
+    @names = ["Local installation", "Remote Talker"]
     sidebarView.dataSource = self
   end
 
@@ -38,6 +58,16 @@ class ApplicationController
 
   def tableView(view, objectValueForTableColumn:column, row:index)
 	@names[index]
+  end
+
+  def growl(message)
+  	GrowlApplicationBridge.notifyWithTitle("Talker",
+		description: message,
+		notificationName: "Test",
+		iconData: nil,
+		priority: 0,
+		isSticky: false,
+		clickContext: nil)
   end
 
 end
