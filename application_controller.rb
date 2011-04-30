37eef32aa0af10732482e5a834d0f32763348c4a
Pablo Villalba <pablo@teambox.com>
2011-04-30 00:09:02 -0700

    Adding asynchronous requests

--- application_controller.rb
+++ application_controller.rb
@@ -2,43 +2,60 @@ require 'json'
 
 class ApplicationController
 
-  # Main web view
-  attr_accessor :webView
+    attr_accessor :app, :webView, :messageForm, :sidebarController, :loginWindow
 
-  # Form to send messages
-  attr_accessor :messageForm
+    # Initialize the app...
+    def awakeFromNib
+        webView.mainFrameURL = "http://asdasdasda.lvh.me:3000/rooms/1059638631"
+        webView.mainFrameURL = "http://teambox.talkerapp.com/rooms/819"
 
-  # Controller for the sidebar
-  attr_accessor :sidebarController
+        # Hitting <return> on the message form will post a message
+        messageForm.action = "send_message:"
 
-  # Initialize the app...
-  def awakeFromNib
-	webView.mainFrameURL = "http://asdasdasda.lvh.me:3000/rooms/1059638631"
+        GrowlApplicationBridge.setGrowlDelegate(self)
+    end
 
-	# Hitting <return> on the message form will post a message
-	messageForm.action = "send_message:"
+    def applicationDidFinishLaunching(n)
+        #app.runModalForWindow loginWindow
+        
+        url = NSURL.URLWithString "https://teambox.talkerapp.com/rooms.json"
+        request = ASIHTTPRequest.requestWithURL url
+        request.addRequestHeader "X-Talker-Token", value:"ea275113713187301a1f520c5772658f4cf617c6"
+        
+        request.startSynchronous
+        error = request.error
+        if !error
+            response = request.responseString
+            puts JSON.parse(response).inspect
+        end
+    end
+    
+    def didReceiveResponse(response)
+        puts "got it!!!!"
+    end
+    
+    def didReceiveData(data)
+        puts "got it!!!!"
+    end    
+    
+    # Post a message to the room using JS in the room itself
+    def send_message(sender)
+        if messageForm.stringValue && messageForm.stringValue.length > 0
+            puts messageForm.stringValue
+            @js = webView.windowScriptObject
+            @js.evaluateWebScript("Talker.sendMessage(#{messageForm.stringValue.to_json})")
+            messageForm.stringValue = ""
+        end
+    end
 
-	GrowlApplicationBridge.setGrowlDelegate(self)
-  end
-
-  # Post a message to the room using JS in the room itself
-  def send_message(sender)
-	if messageForm.stringValue && messageForm.stringValue.length > 0
-	  puts messageForm.stringValue
-  	  @js = webView.windowScriptObject
-	  @js.evaluateWebScript("Talker.sendMessage(#{messageForm.stringValue.to_json})")
-	  messageForm.stringValue = ""
-	end
-  end
-
-  def growl(message)
-  	GrowlApplicationBridge.notifyWithTitle("Talker",
-		description: message,
-		notificationName: "Test",
-		iconData: nil,
-		priority: 0,
-		isSticky: false,
-		clickContext: nil)
-  end
+    def growl(message)
+        GrowlApplicationBridge.notifyWithTitle("Talker",
+            description: message,
+            notificationName: "Test",
+            iconData: nil,
+            priority: 0,
+            isSticky: false,
+            clickContext: nil)
+    end
 
 end
---
+++ login_controller.rb
@@ -0,0 +1,19 @@
+# untitled.rb
+# Talker
+#
+# Created by Pablo Villalba on 29/04/11.
+# Copyright 2011 Teambox. All rights reserved.
+
+
+class LoginController
+
+    attr_accessor :app, :loginWindow, :submitButton
+  
+    def awakeFromNib
+    end
+  
+    def performClick(sender)
+        puts "click click"
+    end
+
+end
